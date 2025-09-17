8b02ea5e2a26ad0ec7bbb8d3014e4e07163a1cda
정민 김 <wjdala02@gmail.com>
2025-09-17 11:43:32 +0900

    초기 커밋.

---
+++ tast/hello.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+void* swapdouble(double* pa, double* pb);
+int test_print_array();
+int tast_swapDouble();
+
+int main(void) {
+	test_print_array();
+
+	return 0;
+	
+
+
+}
+int tast_swapDouble()
+{
+	double a = 3.14;
+	double b = 5.12;
+
+	double* presult = (double*)swapdouble(&a, &b);
+	if (presult = NULL)
+	{
+		printf("뭔가 잘못되었어\n");
+	}
+	else
+	{
+		printf("a = %f, b =%f", a, b);
+	}
+}
+
+void * swapdouble(double* pa, double* pb)
+{
+	//1. 입력 값 정상인지 체크 : null이 아니어야 함!!
+	if (pa ==NULL|| pb == NULL)
+	{
+		return NULL;
+
+
+	}
+	//2. SWAP TWO VALUSE
+	double tmp = *pa;
+	*pa = *pb;
+	*pb = tmp;
+
+	return pa;
+
+}
+
+void print_array_double(double arr[], int sz);
+void print_Array_Double(double* arr, int sz);
+
+
+int test_print_array()
+{
+	double da[5] = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+
+	int size = sizeof(da) / sizeof(da[0]);
+
+	print_array_double(da, size);
+
+	print_Array_Double(&da[0], size);
+
+
+	return 0;
+}
+
+// 기능: 1차원 실수 배열을 입력 받아 콘솔에 출력한다.
+// 함수명: print_array_double
+// 입력: double 타입 1차원 배열 주소
+// 출력: 터미널에 배열의 내용을 예쁘게 출력
+// 반환:없음
+
+
+void print_array_double(double arr[], int sz)
+{
+	
+	for (int i = 0; i < sz; i++, arr++)
+	{
+		printf("%.lf ", arr[i]);
+
+	}
+	printf("\n");
+}
+void print_Array_Double(double* arr, int sz)
+{
+	for (int i = 0; i < sz; i++, arr++)
+	{
+		printf("%lf", *arr);
+
+	}
+	printf("\n");
+
+}
